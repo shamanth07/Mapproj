51d33cf3c1db33fcce781761170d428ebe466f5b
shamanth07 <39122615+shamanth07@users.noreply.github.com>
2023-10-04 19:45:54 -0400

    Update ComputerStore.java

--- sam/ComputerStore.java
+++ sam/ComputerStore.java
@@ -22,6 +22,7 @@ public class ComputerStore {
 		// actions based on the user's choice until the user chooses to quit (option 5).
 		int mainMenu;
 		do {
+			// Display the menu options
 			System.out.println("What do you want to do?\n" + "1.\tEnter new computers (password required)\n"
 					+ "2.\tChange information of a computer (password required)\n"
 					+ "3.\tDisplay all computers by a specific brand\n"
@@ -33,14 +34,17 @@ public class ComputerStore {
 			double price;
 			int compcount;
 			int addedComputers = Computer.findNumberOfCreatedComputers();
+			// Switch statement to handle different menu options
 			switch (mainMenu) {
 			case 1:
+				// Option to enter new computers
 				double prz = 0;
 				System.out.println("Enter the password: ");
 				pswd = kb.next();
 				if (verifyPswd(pswd) == false) {
 					break;
 				}
+				 // Loop to enter details for multiple computers
 				do {
 					System.out.println("Enter number of computers : ");
 					compcount = kb.nextInt();
@@ -48,6 +52,7 @@ public class ComputerStore {
 						System.out.println("Enter positive integer");
 					}
 				} while (compcount <= 0 || compcount > maxComp);
+				 // Loop to enter details for each computer
 				for (int i = 0; i < compcount; i++) {
 					System.out.println("Enter Brand:");
 					String brnd = kb.next();
@@ -60,11 +65,13 @@ public class ComputerStore {
 							System.out.println("Please enter a positive number");
 						}
 					} while (prz <= 0);
+					// Create a Computer object and store it in the inventory array
 					inventory[addedComputers + i] = new Computer(brnd, mdl, prz);
 					Computer.displayComputer(inventory[addedComputers + i]);
 				}
 				break;
 			case 2:
+				// Option to change information of a computer
 				System.out.println("Enter the password: ");
 				pswd = kb.next();
 				if (verifyPswd(pswd) == false) {
@@ -72,37 +79,42 @@ public class ComputerStore {
 				}
 				int choice = 0;
 				int computerNum = 0;
+				// Loop to choose a computer to modify
 				do {
 					System.out.println("Enter the computer number: ");
 					computerNum = kb.nextInt();
 
 				} while (computerNum > Computer.findNumberOfCreatedComputers() || computerNum <= 0);
 				computerNum--;
+				// Display the selected computer's information and provide options to modify it
 				if (computerNum < Computer.findNumberOfCreatedComputers() && computerNum >= 0) {
 					System.out.println("Computer " + (computerNum + 1));
 					System.out.println("Brand: " + inventory[computerNum].getBrand());
 					System.out.println("Model: " + inventory[computerNum].getModel());
 					System.out.println("Price: " + inventory[computerNum].getPrice());
 					System.out.println("Serial Number: " + inventory[computerNum].getSN());
+					// Loop to handle the different modification options
 					do {
 						System.out.println("What information would you like to change?\n" + "1.	brand\n" + "2.	model\n"
 								+ "3.	price\n" + "4.	Sn\n" + "5.	Quit\n" + "Enter your choice >\n");
 						choice = kb.nextInt();
 						switch (choice) {
 						case 1:
+							// Option to update brand
 							System.out.println("Update Brand");
 							String brnd = kb.next();
 							inventory[computerNum].setBrand(brnd);
 							Computer.displayComputer(inventory[computerNum]);
 							break;
 						case 2:
+							// Option to update model
 							System.out.println("Update Model");
 							String model = kb.next();
 							inventory[computerNum].setModel(model);
 							Computer.displayComputer(inventory[computerNum]);
 							break;
 						case 3:
-
+							// Option to update price
 							double prize = 0;
 							do {
 								System.out.println("Enter price:");
@@ -115,7 +127,7 @@ public class ComputerStore {
 							Computer.displayComputer(inventory[computerNum]);
 							break;
 						case 4:
-
+							// Option to update serial number
 							int sn = 0;
 							do {
 								System.out.println("Enter serial number:");
@@ -138,12 +150,13 @@ public class ComputerStore {
 				}
 				break;
 			case 3:
+				// Option to display computers by a specific brand
 				System.out.println("Enter brand name: ");
 				brand = kb.next();
 				findComputersBy(brand, inventory);
 				break;
 			case 4:
-
+				// Option to display computers under a certain price
 				price = 0;
 				do {
 					System.out.println("Enter price:");
@@ -155,6 +168,7 @@ public class ComputerStore {
 				findCheaperThan(price, inventory);
 				break;
 			case 5:
+				// Option to quit the program
 				System.out.println("Thank you");
 				System.exit(0);
 				break;
@@ -167,7 +181,7 @@ public class ComputerStore {
 
 		kb.close();
 	}
-
+	// Function to verify the password
 	public static boolean verifyPswd(String pswd) {
 		Scanner kb = new Scanner(System.in);
 		int loopCount = 1;
